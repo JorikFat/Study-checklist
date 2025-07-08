5e6f00657213fe1a2004b78d2c9fd521dacb2cc4
user <pavlig43@gmail.com>
2025-07-08 22:36:09 +0300

    add Display Course Content Feature

---
+++ displayingcoursecontent/src/main/java/com/pavlig43/displayingcoursecontent/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.pavlig43.displayingcoursecontent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.pavlig43.displayingcoursecontent.ui.DisplayingCourseContentScreen
+import com.pavlig43.displayingcoursecontent.ui.theme.Study_checklistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Study_checklistTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DisplayingCourseContentScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
---
+++ displayingcoursecontent/src/main/java/com/pavlig43/displayingcoursecontent/ui/DisplayingCourseContentViewModel.kt
@@ -0,0 +1,44 @@
+package com.pavlig43.displayingcoursecontent.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class DisplayingCourseContentViewModel:ViewModel() {
+    private val _courseState = MutableStateFlow(Course())
+    val courseState = _courseState.asStateFlow()
+
+
+    fun onCheckedChange(index: Int, isChecked: Boolean) {
+        _courseState.update { course ->
+            course.copy(
+                lessons = course.lessons.map { lesson ->
+                    if (lesson.index == index) {
+                        lesson.copy(isChecked = isChecked)
+                    } else {
+                        lesson
+                    }
+                }
+            )
+        }
+    }
+
+}
+
+data class Course(
+    val name: String = "SOLID",
+    val lessons: List<Lesson> = listOf(
+        "SRP",
+        "OCP",
+        "LSP",
+        "ISP",
+        "DIP",
+    ).mapIndexed { index, lesson -> Lesson(index, lesson) }
+)
+
+data class Lesson(
+    val index: Int,
+    val name: String,
+    val isChecked:Boolean = false
+)
