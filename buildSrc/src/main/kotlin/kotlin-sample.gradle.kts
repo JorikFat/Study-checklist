e2cf3203595c6122f3a20977f1f509d37b9801ec
JorikFat <jorikfat@gmail.com>
2025-07-24 21:00:32 +0300

    connect kotlin-sample script to samples

---
+++ buildSrc/src/main/kotlin/kotlin-sample.gradle.kts
@@ -0,0 +1,65 @@
+import gradle.kotlin.dsl.accessors._e56b5c11ec9916ba4e0efdbfc12dc149.androidTestImplementation
+import gradle.kotlin.dsl.accessors._e56b5c11ec9916ba4e0efdbfc12dc149.debugImplementation
+import gradle.kotlin.dsl.accessors._e56b5c11ec9916ba4e0efdbfc12dc149.testImplementation
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.api.JavaVersion
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.the
+
+val libs = the<LibrariesForLibs>()
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.viewmodel.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
