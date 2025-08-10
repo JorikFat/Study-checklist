7cf756eac5207b26d0b9be890254df792719c401
JorikFat <jorikfat@gmail.com>
2025-08-10 19:43:34 +0300

    change: simplify course edit sample

--- features/course-edit/sample/src/main/java/com/pavlig43/courceediting/CourseEditActivity.kt
+++ features/course-edit/sample/src/main/java/com/pavlig43/courceediting/CourseEditActivity.kt
@@ -7,13 +7,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import com.pavlig43.courceediting.ui.CourseEditingScreen
 import com.pavlig43.courceediting.ui.theme.Study_checklistTheme
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.parameter.parametersOf
+import org.koin.dsl.module
+import ru.pavlig.course_edit.ui.Course
+import ru.pavlig.course_edit.ui.CourseEditingLayout
+import ru.pavlig.course_edit.ui.CourseEditingViewModel
+import ru.pavlig.course_edit.ui.Lesson
 
 class CourseEditActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(savedInstanceState == null){
+            startKoin {
+                androidLogger()
+                androidContext(application)
+                modules(
+                    module {
+                        viewModel { (course: Course) ->
+                            CourseEditingViewModel(course)
+                        }
+                    }
+                )
+            }
+        }
         enableEdgeToEdge()
         setContent {
             Study_checklistTheme {
@@ -28,6 +54,32 @@ class CourseEditActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun CourseEditingScreen(
+    modifier: Modifier = Modifier,
+    name: String? = null
+) {
+    val course: Course = if(name == null) Course() else sampleCourse
+    val viewModel: CourseEditingViewModel = koinViewModel { parametersOf(course) }
+    val courseState by viewModel.courseState.collectAsState()
+    CourseEditingLayout(
+        course = courseState,
+        viewModel = viewModel,
+        modifier = modifier,
+    )
+}
+
+private val sampleCourse = Course(
+    name = "SOLID",
+    lessons = listOf(
+        "SRP",
+        "OCP",
+        "LSP",
+        "ISP",
+        "DIP",
+    ).mapIndexed { index, lesson -> Lesson(index, lesson) }
+)
 
 
 
