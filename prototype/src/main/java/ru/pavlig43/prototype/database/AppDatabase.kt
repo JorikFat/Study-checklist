bfc685efbb16b1d5021cfe73c43c93857781253b
highscopeDER <highscope@mail.ru>
2025-08-20 11:45:03 +0300

    add database class

---
+++ prototype/src/main/java/ru/pavlig43/prototype/database/AppDatabase.kt
@@ -0,0 +1,20 @@
+package ru.pavlig43.prototype.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.pavlig43.prototype.database.dao.DAO
+import ru.pavlig43.prototype.database.entities.CourseContentEntity
+import ru.pavlig43.prototype.database.entities.CourseEntity
+import ru.pavlig43.prototype.database.entities.LessonEntity
+
+@Database(
+    entities = [
+        CourseEntity::class,
+        LessonEntity::class,
+        CourseContentEntity::class
+    ],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun getDao(): DAO
+}
