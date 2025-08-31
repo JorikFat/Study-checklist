eee5277af7b1e5a92c9c38e78d780ebb2e224dde
JorikFat <jorikfat@gmail.com>
2025-08-31 18:59:39 +0300

    fix: add delete callback

--- features/course-edit/lib/src/androidTest/java/ru/pavlig/course_edit/UITest.kt
+++ features/course-edit/lib/src/androidTest/java/ru/pavlig/course_edit/UITest.kt
@@ -35,6 +35,7 @@ class UITest {
                 onAddLesson = {},
                 onDeleteLesson = {},
                 onSave = {},
+                onDeleteCourse = {},
                 onNavigateBack = {},
             )
         }
