d3a9ab11eb2fd66d79b2fba9f78cea6869eca31e
JorikFat <jorikfat@gmail.com>
2025-09-03 20:36:25 +0300

    add: fake repository

---
+++ features/courses/src/main/java/com/example/courses/database/repository/FakeCoursesRepository.kt
@@ -0,0 +1,74 @@
+package com.example.courses.database.repository
+
+import com.example.courses.models.Course
+import com.example.courses.models.Lesson
+
+class FakeCoursesRepository :CoursesRepository {
+
+    private val stubLessons: MutableList<Lesson> = mutableListOf(
+        Lesson(1, "SRP"),
+        Lesson(2, "OCP"),
+        Lesson(3, "LSP"),
+        Lesson(4, "ISP"),
+        Lesson(5, "DIP"),
+    )
+
+    private val stubCourses :MutableList<Course> = mutableListOf(
+        Course(1,"SOLID", stubLessons),
+        Course(2,"Clean Architecture", listOf(Lesson(6,"Clean lesson"))),
+        Course(3,"Design Patterns", listOf(Lesson(7,"DP lesson")))
+    )
+
+    override suspend fun getCourses(): List<Course> =
+        stubCourses.toList()
+
+    override suspend fun courseCreate(course: Course) {
+        val lastId = stubCourses.lastOrNull()?.id ?: 0
+        stubCourses.add(course.copy(id = lastId + 1))
+    }
+
+    override suspend fun courseDelete(course: Course) {
+        stubCourses.removeIf { it.id == course.id }
+    }
+
+    override suspend fun courseUpdate(course: Course) {
+        val courseIndex = courseIndexById(course.id)
+        stubCourses[courseIndex] = course
+    }
+
+    override suspend fun lessonCreate(courseId: Int, lesson: Lesson) {
+        val courseIndex = courseIndexById(courseId)
+        val course = stubCourses[courseIndex]
+        val lessons = course.lessons.toMutableList()
+        val lastId = lessons.lastOrNull()?.id ?: 0
+        lessons.add(lesson.copy(id = lastId + 1))
+        stubCourses[courseIndex] = course.copy(lessons = lessons)
+    }
+
+    override suspend fun lessonDelete(courseId: Int, lesson: Lesson) {
+        val courseIndex = courseIndexById(courseId)
+        val course = stubCourses[courseIndex]
+        val lessons = course.lessons.toMutableList()
+        val lessonIndex = lessons.indexOfFirst { it.id == lesson.id }
+        lessons.removeAt(lessonIndex)
+        stubCourses[courseIndex] = course.copy(lessons = lessons)
+    }
+
+    override suspend fun lessonUpdate(courseId: Int, lesson: Lesson) {
+        val courseIndex = courseIndexById(courseId)
+        val course = stubCourses[courseIndex]
+        val lessons = course.lessons.toMutableList()
+        val lessonIndex = lessons.indexOfFirst { it.id == lesson.id }
+        lessons[lessonIndex] = lesson
+        stubCourses[courseIndex] = course.copy(lessons = lessons)
+    }
+
+    private fun courseIndexById(id :Int) :Int = stubCourses.indexOfFirst { it.id == id }
+}
+
+fun <T, R> List<T>.replaceFirstBy(item :T, selector: (T)->R) : List<T> {
+    val mutable = toMutableList()
+    val index = indexOfFirst { selector(item) == selector(it) }
+    mutable[index] = item
+    return mutable.toList()
+}
