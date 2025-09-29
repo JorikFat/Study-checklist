7f15e211655d20e45fc19365330f2624dcbd81b7
JorikFat <jorikfat@gmail.com>
2025-09-29 17:15:39 +0300

    refactor: initialize course interactor

--- features/courses/src/main/java/com/example/courses/CourseInteractor.kt
+++ features/courses/src/main/java/com/example/courses/CourseInteractor.kt
@@ -7,16 +7,18 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.runBlocking
 
 class CourseInteractor(
     private val coursesRepository: CoursesRepository,
 ) {
-    private val _courseMenuList : StateFlow<List<Course>> = coursesRepository.listen().stateIn(
-        GlobalScope,
-        SharingStarted.Eagerly,
-        initialValue = emptyList()
-    )
-    val courseMenuList :Flow <List<Course>> = _courseMenuList
+    private val _courseMenuList: StateFlow<List<Course>> = coursesRepository.listen()
+        .stateIn(
+            GlobalScope,
+            SharingStarted.Eagerly,
+            initialValue = runBlocking { coursesRepository.getCourses() }
+        )
+    val courseMenuList: Flow<List<Course>> = _courseMenuList
 
     fun findCourseById(id: Int): Course? {
         return _courseMenuList.value.find { it.id == id }
